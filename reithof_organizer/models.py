0cf6c4b43a3d90fd309e7e8ffddc9c688e4fc9da
AI2211 <tolik1990@hotmail.de>
2018-11-29 19:57:33 +0100

    Pferd und Besitzer Model hinzugefügt

--- reithof_organizer/models.py
+++ reithof_organizer/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Besitzer(models.Model):
+    vorname = models.CharField(max_length=100)
+    nachname = models.CharField(max_length=100)
+    geburtstag = models.DateTimeField(blank=True)
+    erstellt_am = models.DateTimeField(auto_now_add=True)
+    mistpunkte = models.IntegerField(max_length=50)
+
+class Pferd(models.Model):
+    offizieller_name = models.CharField(max_length=100)
+    rufname = models.CharField(max_length=100)
+    geburtstag = models.DateTimeField(blank=True)
+    kategorie = models.CharField(max_length=100)
+
