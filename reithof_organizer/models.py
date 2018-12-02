f5ac70a27e3c135de18e5c2c59ac54e4251a94a8
AI2211 <tolik1990@hotmail.de>
2018-12-02 17:49:10 +0100

    admin interface

--- reithof_organizer/models.py
+++ reithof_organizer/models.py
@@ -1,12 +1,5 @@
 from django.db import models
 
-class Person(models.Model):
-    pferd = models.ForeignKey(on_delete=models.DO_NOTHING)
-    vorname = models.CharField(max_length=100)
-    nachname = models.CharField(max_length=100)
-    geburtstag = models.DateTimeField(blank=True)
-    erstellt_am = models.DateTimeField(auto_now_add=True)
-    mistpunkte = models.IntegerField()
 
 class Pferd(models.Model):
     offizieller_name = models.CharField(max_length=100)
@@ -14,3 +7,10 @@ class Pferd(models.Model):
     geburtstag = models.DateTimeField(blank=True)
     kategorie = models.CharField(max_length=100)
 
+class Person(models.Model):
+    pferd = models.ForeignKey('Pferd', on_delete=models.PROTECT)
+    vorname = models.CharField(max_length=100)
+    nachname = models.CharField(max_length=100)
+    geburtstag = models.DateTimeField(blank=True)
+    erstellt_am = models.DateTimeField(auto_now_add=True)
+    mistpunkte = models.IntegerField()
