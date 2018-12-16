1ebdc1c37d1f8970c61b7f9c5d6141f151bfa8e9
FabyP <fabienne.pipping@iem.thm.de>
2018-12-16 15:45:24 +0100

    Files für Heroku vorbereitet

--- reithof_site/settings.py
+++ reithof_site/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -127,3 +128,6 @@ STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'reithof_organizer.Profile'
 
 LOGIN_REDIRECT_URL = 'ritterstall'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
