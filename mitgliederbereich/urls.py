9cd75f56c4362de67626151b11ecd8b3a742bf04
Margaret Woltschanski <margaret.woltschanski@iem.thm.de>
2018-12-17 18:51:54 +0100

    update mitglieder

--- mitgliederbereich/urls.py
+++ mitgliederbereich/urls.py
@@ -2,5 +2,15 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='profil'),
+
+    #Mistplan
+    url(r'^mistplan/$', views.mistplan, name='mistplan'),
+
+    #StandortPferde
+    url(r'^standortPferde/$', views.standortPferde, name='standortPferde'),
+
+    #Termine
+    url(r'^termine/$', views.termine, name='termine'),
+
 ]
--- mitgliederbereich/views.py
+++ mitgliederbereich/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'mitgliederbereich/index.html')
+
+def mistplan(request):
+    return render(request, 'mitgliederbereich/mistplan.html')
+
+def standortPferde(request):
+    return render(request, 'mitgliederbereich/standortPferde.html')
+
+def termine(request):
+    return render(request, 'mitgliederbereich/termine.html')
+
